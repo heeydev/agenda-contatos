88754681d4165ec906f6bdb1371e1111428d56f9
Hiago Crespo <maciellbranco@hotmail.com>
2024-10-23 13:23:07 -0300

    ajustando caminho da rota da api

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::apiResource('contatos', 'App\Http\Controllers\Api\ContactController');
+Route::apiResource('contatos', 'App\Http\Controllers\api\ContactController');
